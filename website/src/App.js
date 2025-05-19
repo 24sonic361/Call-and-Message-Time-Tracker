2e0ff6ccc43cc5f2f748319d437f858387c68645
mizzie20 <144868252+vcb3635@users.noreply.github.com>
2025-05-19 20:28:04 +1200

    Billpage visible

--- website/src/App.js
+++ website/src/App.js
@@ -27,6 +27,7 @@ const App = () => (
           <Route path="/login" element={<LoginPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route path="/request" element={<RequestAccountPage />} />
+          <Route path="/bill" element={<BillPage />} />
 
           {/* Private Routes */}
           <Route 
