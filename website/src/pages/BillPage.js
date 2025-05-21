49873e5ccb3cf3b256bb7e3391244ea5b79208d3
mizzie20 <144868252+vcb3635@users.noreply.github.com>
2025-05-22 11:09:46 +1200

    Update BillPage.js

--- website/src/pages/BillPage.js
+++ website/src/pages/BillPage.js
@@ -4,6 +4,7 @@ import {Table,Form,Button,Card,Row,Col,} from "react-bootstrap";
 import { supabase } from "../supabaseClient";
 import Sidebar from "../components/Sidebar";
 import  "../styles/HomePage.css";
+import "../styles/BillPage.css";
 
 const BillPage = () => {
   const [callLogs, setCallLogs] = useState([]);
