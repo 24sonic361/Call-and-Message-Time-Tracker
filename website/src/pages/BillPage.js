bfd9a101727145916450b9d62d714d54da791239
mizzie20 <144868252+vcb3635@users.noreply.github.com>
2025-05-22 15:28:33 +1200

    Update BillPage.js

--- website/src/pages/BillPage.js
+++ website/src/pages/BillPage.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import {Table,Form,Button,Card,Row,Col,} from "react-bootstrap";
 import { supabase } from "../supabaseClient";
 import Sidebar from "../components/Sidebar";
-import "../styles/HomePage.css";
 import "../styles/BillPage.css";
 import Swal from "sweetalert2";
 import { useAuth } from "../AuthProvider"; // Import useAuth
